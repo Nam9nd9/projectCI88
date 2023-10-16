fbb0ae162ebcc2e425f6ee253736032264a3cf23
mai van nam <maivannam27699@gmai.com>
2023-10-16 21:39:02 +0700

    adsdsad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Route, Routes} from "react-router-dom"
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ProductDetail from './pages/ProductDetail'
+import Cart from './pages/Cart'
+import Wishlist from './pages/Wishlist'
+import Payment from './pages/Payment'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/wishlist" element={<Wishlist/>}/>
+      <Route path="/products" element={<ProductDetail/>}/>
+      <Route path="/cart" element={<Cart />}/>
+      <Route path="/payment" element={<Payment/>}/>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
